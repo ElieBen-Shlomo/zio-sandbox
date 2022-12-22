21f4dfab2cd6039c5954dd09be470a91a7ee67c0
Elie Ben-Shlomo <benshlomoelie@gmail.com>
2022-12-22 13:57:11 +0000

    Migrating to Scala 2

--- src/main/scala/Data.scala
+++ src/main/scala/Data.scala
@@ -1,70 +1,64 @@
-package main
+package cassandra_quill
 
 import io.getquill._
+import io.getquill.ast.Ast
 import io.getquill.cassandrazio.Quill
 import zio.Console.printLine
-import zio.{ZIO, ZIOAppDefault, ZLayer}
+import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 
-import java.io.IOException
-import java.sql.Timestamp
 import java.time.Instant
 
 object Data {
 
   case class User(
-    username: String,
-    date_and_time: Instant,
-    message: String,
-    session_id: String,
-    status: String // replace with enum
+      username: String,
+      dateAndTime: Instant,
+      message: String,
+      sessionId: String,
+      status: String
   )
 
-  case class QueryService(quill: Quill.Cassandra[Literal]) {
-    import quill.*
-
-    inline def people = quote {
+  case class QueryService(quill: Quill.Cassandra[SnakeCase]) {
+    import quill._
+    def people = quote {
       query[User]
     }
-    inline def peopleByName = quote {
-      (name: String) => people.filter(p => p.username == name).allowFiltering
-    }
-    inline def insertPerson = quote {
-      (user: User) => query[User].insertValue(user)
-    }
-  }
 
+    def peopleByName = quote { (name: String) =>
+      people.filter(p => p.username == name).allowFiltering
+    }
+
+    def insertPerson = quote { (user: User) =>
+      query[User].insertValue(user)
+    }
+
+  }
   object QueryService {
-    def live: ZLayer[Quill.Cassandra[Literal], Nothing, QueryService] =
+    def live: ZLayer[Quill.Cassandra[SnakeCase], Nothing, QueryService] =
       ZLayer.fromFunction(QueryService(_))
   }
 
   case class DataService(queryService: QueryService) {
-
+    import queryService.quill._
     import queryService.quill
-    import queryService.quill.*
-
-    def getPeople(): ZIO[Any, Throwable, List[User]] = {
-      quill.run(queryService.people)
-    }
-
-    def getPeopleByName(name: String): ZIO[Any, Throwable, List[User]] = quill.run(queryService.peopleByName(lift(name)))
-
+    def getPeople: ZIO[Any, Throwable, List[User]] = quill.run(queryService.people)
+    def getPeopleByName(name: String): ZIO[Any, Throwable, List[User]] =
+      quill.run(queryService.peopleByName(lift(name)))
     def insertPerson(user: User): ZIO[Any, Throwable, Unit] = {
       quill.run(queryService.insertPerson(lift(user)))
     }
   }
 
   object DataService {
-    def getPeople(): ZIO[DataService, Throwable, List[User]] =
-      ZIO.serviceWithZIO[DataService](_.getPeople())
-
+    def getPeople: ZIO[DataService, Throwable, List[User]] =
+      ZIO.serviceWithZIO[DataService](_.getPeople)
     def getPeopleByName(name: String): ZIO[DataService, Throwable, List[User]] =
       ZIO.serviceWithZIO[DataService](_.getPeopleByName(name))
-
     def insertPerson(user: User): ZIO[DataService, Throwable, Unit] =
       ZIO.serviceWithZIO[DataService](_.insertPerson(user))
 
     def live: ZLayer[QueryService, Nothing, DataService] =
       ZLayer.fromFunction(DataService(_))
   }
+
 }
--- src/main/scala/HttpServer.scala
+++ src/main/scala/HttpServer.scala
@@ -1,18 +1,15 @@
 package main
 
-import scala.io.StdIn.readLine
-import zio.*
-import zio.{Console, ZIOAppDefault}
-import zio.*
-import zio.http.ChannelEvent.{ChannelRead, ChannelUnregistered, UserEventTriggered}
-import zio.http.*
+import cassandra_quill.Data
+import cassandra_quill.Data.DataService
+import zio.{Console, ZIO, ZLayer}
 import zio.http.model.Method
-import zio.http.socket.{WebSocketChannelEvent, WebSocketFrame}
-import zio.http.ChannelEvent.UserEvent.*
+import zio.http.{Http, Request, Response, Server, ServerConfig}
+import zio.http._
 
-import Data.DataService
+import java.io.IOException
 
-object HttpServer:
+object HttpServer {
 
   private val websocket: Http[DataService, Nothing, Request, Response] = Http.collectZIO[Request] {
     case Method.GET -> !! / "ws" => WebSocket.beginWebsocketConnection.toSocketApp.toResponse
@@ -21,8 +18,10 @@ object HttpServer:
   private val port = 9000
 
   val configLayer: ZLayer[Any, Nothing, ServerConfig] = ServerConfig
-      .live(ServerConfig.default.port(port))
+    .live(ServerConfig.default.port(port))
 
-  val startServer =
+  val startServer: ZIO[DataService with Server, IOException, Nothing] =
     Console.printLine(s"Starting server on http://localhost:$port") *>
       Server.serve(websocket)
+
+}
